80d146b9ba3aae7f67afcfe8e1cdbef0225df834
shreejithora <30403846+shreejithora@users.noreply.github.com>
2020-02-01 16:00:33 +0545

    guide and tourist register model made

--- BookMyGuide/user_app/admin.py
+++ BookMyGuide/user_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import GuideRegisterModel, TouristRegisterModel
 
 # Register your models here.
+
+admin.site.register(GuideRegisterModel)
+admin.site.register(TouristRegisterModel)
--- BookMyGuide/user_app/models.py
+++ BookMyGuide/user_app/models.py
@@ -1,3 +1,67 @@
 from django.db import models
+from multiselectfield import MultiSelectField
 
 # Create your models here.
+
+class GuideRegisterModel(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+    bio_desc = models.TextField(max_length=225)
+    email = models.EmailField()
+    password1 = models.CharField(max_length=50)
+    password2 = models.CharField(max_length=50)
+    GENDER_MALE = 0
+    GENDER_FEMALE = 1
+    GENDER_OTHERS = 2
+    GENDER_CHOICES = [
+        (GENDER_MALE, 'Male'),
+        (GENDER_FEMALE, 'Female'),
+        (GENDER_OTHERS, 'Others'),
+    ]
+    gender = models.IntegerField(choices=GENDER_CHOICES)
+    address = models.CharField(max_length=50)
+    contact = models.CharField(max_length=50)
+    user_img = models.ImageField(upload_to='UserImg', blank=True, null=True)
+    citizenship_img = models.ImageField(upload_to='Citizenship')
+    Liscence_img = models.ImageField(upload_to='LiscenceImg')
+    LANGUAGES = (
+        ('Nepali', 'Nepali'),
+        ('English', 'English'),
+        ('Spanish', 'Spanish'),
+        ('Hindi', 'Hindi'),
+        ('Chinese', 'Chinese'),
+    )
+    languages = MultiSelectField(choices=LANGUAGES)
+    L_CHOICES = (
+        ('Kathmandu', 'Kathmandu'),
+        ('Bhaktapur', 'Bhaktapur'),
+        ('Lalitpur', 'Lalitpur'),
+        ('Everest Base Camp', 'Everest Base Camp'),
+        ('Annapurna Base Camp', 'Annapurna Base Camp'),
+    )
+    locations = MultiSelectField(choices=L_CHOICES)
+    is_guide = models.BooleanField(default=1)
+
+    def __str__(self):
+        return(self.username)
+    
+class TouristRegisterModel(models.Model):
+    username = models.CharField(max_length=50)
+    email = models.EmailField()
+    password1 = models.CharField(max_length=50)
+    password2 = models.CharField(max_length=50)
+    GENDER_MALE = 0
+    GENDER_FEMALE = 1
+    GENDER_OTHERS = 2
+    GENDER_CHOICES = [
+        (GENDER_MALE, 'Male'),
+        (GENDER_FEMALE, 'Female'),
+        (GENDER_OTHERS, 'Others'),
+    ]
+    gender = models.IntegerField(choices=GENDER_CHOICES)
+    is_tourist = models.BooleanField(default=1)
+
+    def __str__(self):
+        return(self.username)
+
