192309194e4d390c5a2ba43f5a921289e5cf06e8
shreejithora <30403846+shreejithora@users.noreply.github.com>
2020-02-01 22:41:18 +0545

    added bmg and destination app

--- BookMyGuide/user_app/urls.py
+++ BookMyGuide/user_app/urls.py
@@ -14,8 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from user_app import urls
+from django.urls import path
 
 urlpatterns = [
 ]
---
+++ bmg-env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\r\desktop\blue\bmg-env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
