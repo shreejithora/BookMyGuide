b4574f92c9d454d245d9dc02339551bb7753412f
shreejithora <30403846+shreejithora@users.noreply.github.com>
2020-02-11 15:38:10 +0545

    login backend and signup

--- BookMyGuide/destination_app/views.py
+++ BookMyGuide/destination_app/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
+from .models import HireModel
 
 # Create your views here.
 
 def hireform(request):
-    return render(request, 'hireform.html')
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        pick_date = request.POST['pick_date']
+        full_address = request.POST['full_address']
+        phone = request.POST['phone']
+        tour = request.POST['tour']
+
+        hire = HireModel.object.create(username=username,
+                                        email=email,
+                                        pick_date=pick_date,
+                                        full_address=full_address,
+                                        address=address,
+                                        phone=phone,
+                                        tour=tour)
+        hire.save()
+    else:
+        return render(request, 'hireform.html')
 
 def findaguide(request):
     return render(request, 'findaguide.html')
--- BookMyGuide/user_app/views.py
+++ BookMyGuide/user_app/views.py
@@ -1,14 +1,77 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import GuideRegisterModel, TouristRegisterModel
 
 # Create your views here.
 
 def loginauth(request):
-    return render(request, 'login.html')
+    # if request.method == "POST":
+    #     e = request.POST.get('email')
+    #     p = request.POST.get('password')
+    #     guide = GuideRegisterModel.objects.filter(email=e, password=p, is_guide=True)
+    #     tourist = TouristRegisterModel.objects.filter(email=e, password=p, is_tourist=True)
+    #     if(guide.count() > 0 or tourist.count() > 0 ):
+    #         if(guide.is_guide == True):
+    #             for user in guide:
+    #                 request.session['email'] = user.email
+    #                 request.session['id'] = user.id
+    #                 request.session['username'] = user.username
+    #                 return redirect('bmg_app:home')
+    #         elif(tourist.is_tourist == True):
+    #             for user in tourist:
+    #                 request.session['email'] = user.email
+    #                 request.session['id'] = user.id
+    #                 request.session['username'] = user.username
+    #                 return redirect('bmg_app:home')
+    #     else:
+    #         return HttpResponse('Wrong Credentials')
+    # else:
+        return render(request, 'login.html')
 
 def signup_guide(request):
-    return render(request, 'signup_guide.html')
+    if request.method == "POST":
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        bio_desc = request.POST['bio_desc']
+        email = request.POST['email']
+        password = request.POST['password']
+        password1 = request.POST['password1']
+        gender = request.POST['gender']
+        address = request.POST['address']
+        phone = request.POST['phone']
+        user_img = request.POST['user_img']
+        citizenship_img = request.POST['citizenship_img']
+        liscence_img = request.POST['liscence']
+        languages = request.POST['languages']
+        locations = request.POST['location']
+        is_guide - request.POST['is_guide']
+        if is_guide == 'on':
+            return True
+        elif is_guide == 'off':
+            return False
+
+        guide = GuideRegisterModel.objects.create(first_name=first_name, last_name=last_name, username=username, bio_desc=bio_desc, email=email, password=password, gender=gender, address=address, phone=phone, user_img=user_img, citizenship_img=citizenship_img, liscence_img=liscence_img, languages=languages, locations=locations, is_guide=is_guide)
+        # guide.save()
+        guide.save.m2m()
+
+    else:
+        return render(request, 'signup_guide.html')
 
 def signup_tourist(request):
-    # return HttpResponse('hello')
-    return render(request, 'signup_tourist.html')
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        gender = request.POST['gender']
+        is_tourist = request.POST['is_tourist']
+        if is_tourist == 'on':
+            return True
+        elif is_tourist == 'off':
+            return False
+
+        tourist = TouristRegisterModel.objects.create(username=username, email=email, password=password, gender=gender, is_tourist=is_tourist)
+        tourist.save.m2m()
+
+    else:
+        return render(request, 'signup_tourist.html')
